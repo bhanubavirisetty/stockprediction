7febc4208ddb5b2227f0237f2d50cddd71d735cf
Bhanu <bnpavankumar.bavirisetty1998@gmail.com>
2025-01-27 23:34:38 -0600

    Concatenated the Date+Stock_Name and Deleted the old data with new delete call

--- datafetch.py
+++ datafetch.py
@@ -4,20 +4,27 @@ from pymongo import MongoClient
 
 def fetch_data():
     reliance_stock=yf.Ticker("RELIANCE.NS")
-
     stock_history=reliance_stock.history("1mo")
     stock_history.reset_index(inplace=True)
-
     sorted_stock_data=stock_history.sort_values(by='Date',ascending=False)
-
-    return sorted_stock_data
-    
+    #Fetching the Date Column and converting the Date Col datatype (Date) to String
+    sorted_stock_data['StockId']=sorted_stock_data['Date'].dt.date.astype(str)
+    sorted_stock_data['StockId']=sorted_stock_data["StockId"]+"RELIANCE.NS"
+    #Placing the Unique ID in the first column
+    stock_date_col = sorted_stock_data.pop("StockId")  
+    sorted_stock_data.insert(0, "Stock_Id", stock_date_col) 
+    print(sorted_stock_data)
+    return sorted_stock_data   
     #sorted_stock_data.to_csv("RelianceIndustries.csv",index=False)
 
 
 if __name__=="__main__":
     sorted_stock_data=fetch_data()
-    print(sorted_stock_data)
+    
+
+
+
+
 
 
 
--- dataingestion.py
+++ dataingestion.py
@@ -1,27 +1,34 @@
 from pymongo import MongoClient
 from datafetch import fetch_data
 
-#Insertion Of Data into MongoDb
-def insert_data_to_mongo(data,client,db,collection):
-    client=MongoClient(connection_string)
-    db_name=client[db]
-    collection_name=db_name[collection]
+# Insertion of Data into MongoDb
+def insert_data_to_mongo(data, connection_string, db, collection):
+    client = MongoClient(connection_string)
+    db_name = client[db]
+    collection_name = db_name[collection]
 
-    #Insert all the Stock Data into MongoDB Atlas
+    # Insert all the Stock Data into MongoDB Atlas
     collection_name.insert_many(data.to_dict(orient="records"))
 
-    #Documents Count inside the MongoDB
-    query_count=collection_name.count_documents({})
+    # Documents Count inside the MongoDB
+    query_count = collection_name.count_documents({})
     print(f"The total number of documents {query_count}")
 
-    
+# Delete all documents
+def deleteAll(connection_string, db, collection):
+    client = MongoClient(connection_string)
+    db_name = client[db]
+    collection_name = db_name[collection]
 
-#MongoDB Atlas Connection Details
-connection_string="mongodb+srv://bnpavankumarbavirisetty1998:Cloud123@stock-data-cluster.f0cjt.mongodb.net/?retryWrites=true&w=majority&appName=stock-data-cluster"
-db="stockdb"
-collection="stockcollection"
-    
+    # Delete all documents from the collection
+    collection_name.delete_many({})
 
-if __name__=="__main__":
-    data=fetch_data()
-    insert_data_to_mongo(data,connection_string,db,collection)
+# MongoDB Atlas Connection Details
+connection_string = "mongodb+srv://bnpavankumarbavirisetty1998:Cloud123@stock-data-cluster.f0cjt.mongodb.net/?retryWrites=true&w=majority&appName=stock-data-cluster"
+db = "stockdb"
+collection = "stockcollection"
+
+if __name__ == "__main__":
+    data = fetch_data()
+    insert_data_to_mongo(data, connection_string, db, collection)
+    #deleteAll(connection_string, db, collection)
